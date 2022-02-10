320e6e248ef1bb9a33f8beccc477c8f68112cca1
Fatih Eğriboz <fatih.egriboz@bilgi.edu.tr>
2022-02-10 12:05:25 +0300

    page setPage

--- src/App.js
+++ src/App.js
@@ -1,62 +1,61 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import InfiniteScroll from "react-infinite-scroll-component";
 
-// const productsGrid = {
-//   width: '100%',
-//   height: 300,
-//   border: "1px solid green",
-//   flex: '0 0 25%',
-//   margin: 6,
-//   padding: 8
-// };
-
-const App = ({ widget }) => {
+function App({ widget }) {
   const fetchUrl = widget.getAttribute("data-url");
-  const lang = widget.getAttribute("data-lang");
+  // const lang = widget.getAttribute("data-lang");
 
-  //const apiUrl = "rickandmortyapi.com/api/character/?page";
+  // const fetchUrl = "rickandmortyapi.com/api/character/?page";
   // console.log("lang", lang);
-  console.log("endpoint:", fetchUrl, "/ lang:", lang);
+  // console.log("endpoint:", fetchUrl, "/ lang:", lang);
 
   const [items, setItems] = useState([]);
   const [hasMore, sethasMore] = useState(true);
-  const [page, setpage] = useState(1);
+  // const [page, setpage] = useState(1);
+  const [page, setpage] = useState(2);
+  console.log("page-first", page);
 
-  useEffect(() => {
+
+  useEffect(async () => {
     const getFirstProducts = async () => {
       const res = await fetch(`https://${fetchUrl}=1`);
       const dataJson = await res.json();
       const data = dataJson.results;
       setItems(data);
-    };
-
+      console.log("page-first 2", page);
+    }
     getFirstProducts();
+    console.log("page-first 3", page);
   }, []);
 
   const getFetchProducts = async () => {
+    console.log("getFetchProducts page:", page);
     const res = await fetch(`https://${fetchUrl}=${page}`);
     const dataJson = await res.json();
     const data = dataJson.results;
     return data;
   };
 
-  const fetchData = async () => {
+  const fetchDataNext = async () => {
+    console.log("fetchDataNext page-first fd 4", page);
     setTimeout(async () => {
       const scrollingItem = await getFetchProducts();
       console.log("scrollingItem:", scrollingItem);
+
       setItems([...items, ...scrollingItem]);
+      setpage(Number(page) + 1);
 
       if (scrollingItem.length === 0 || scrollingItem.length < 20) {
         sethasMore(false);
       }
-      setpage(page + 1);
+
     }, 900);
   };
 
   return (
     <InfiniteScroll
       dataLength={items.length}
-      next={fetchData}
+      next={fetchDataNext}
       hasMore={hasMore}
       loader={<h4>Loading...</h4>}
       endMessage={<p>end</p>}
@@ -65,7 +64,7 @@ const App = ({ widget }) => {
       {items.map((i, index) => (
         <article className="article" key={index}>
           <div className="inner">
-            <img src={i.image} alt={i.name} title={i.name} />#{i.id} {i.name}
+            <img src={i.image} alt={i.name} title={i.name} /><strong>#{i.id}</strong> {i.name}
           </div>
         </article>
       ))}
